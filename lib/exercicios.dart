bf3822b2109b5a7c271b75000a681b1a7eb9e8f8
Juliano Coelho <julianocoelho448@gmail.com>
2026-03-01 21:17:52 -0300

    Atividades Dart

--- lib/exercicios.dart
+++ lib/exercicios.dart
@@ -1,49 +1,91 @@
-// 1. Soma
-num somar(num a, num b){
-  return 0;
+// 1. Deve somar dois números
+int somar(int a, int b) {
+  return a + b;
 }
 
-// 2. Par
-bool isPar(int n) {
+// 2. Deve verificar se é par
+bool isPar(int numero) {
+  if (numero % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// 3. Deve validar maioridade
+bool podeEntrar(int idade) {
+  if (idade >= 18) {
+    return true;
+  }
   return false;
 }
 
-// 3. Maior de idade
-bool podeEntrar(int idade){
-  return false;
+// 4. Deve converter Celsius para Fahrenheit
+double converter(num celsius) {
+  return (celsius * 1.8) + 32.0;
 }
 
-// 4. Celsius para Fahrenheit
-double converter(double c){
-  return 0.0;
+// 5. Deve saudar o usuário
+String saudar(String nome) {
+  return "Olá, $nome!";
 }
 
-// 5. Saudação
-String saudar(String nome){
-  return "";
+// 6. Deve calcular a média da lista
+double calcularMedia(List<num> numeros) {
+  if (numeros.isEmpty) {
+    return 0.0;
+  }
+
+  double soma = 0;
+  for (int i = 0; i < numeros.length; i++) {
+    soma = soma + numeros[i];
+  }
+
+  return soma / numeros.length;
 }
 
-// 6. Média
-double calcularMedia(List<double> notas) {
-  return 0.0;
+// 7. Deve filtrar números maiores que o corte
+List<int> buscarMaiores(List<int> numeros, int corte) {
+  List<int> resultado = [];
+
+  for (int i = 0; i < numeros.length; i++) {
+    if (numeros[i] > corte) {
+      resultado.add(numeros[i]);
+    }
+  }
+
+  return resultado;
 }
 
-// 7. Filtrar Maiores
-List<int> buscarMaiores(List<int> numeros, int corte){
-  return [];
+// 8. Deve lidar com Strings nulas
+int tamanhoTexto(String? texto) {
+  if (texto == null) {
+    return 0;
+  }
+  return texto.length;
 }
 
-// 8. Nulos (Null Safety)
-int tamanhoTexto(String? texto){
-  return 0;
+// 9. Deve calcular pedido com descontos progressivos
+double fecharPedido(List<num> valores) {
+  double total = 0;
+
+  for (int i = 0; i < valores.length; i++) {
+    total = total + valores[i];
+  }
+
+  if (total > 500) {
+    return total - (total * 0.15);
+  } else {
+    return total - (total * 0.05);
+  }
 }
 
-// 9. Carrinho com Desconto
-double fecharPedido(List<double> precos){
-  return 0.0;
-}
-
-// 10. Busca Case Insensitive
-bool contemNome(List<String> nomes, String busca){
+// 10. Deve buscar nome na lista (Insensivel a Caso Final)
+bool contemNome(List<String> lista, String nomeBuscado) {
+  for (int i = 0; i < lista.length; i++) {
+    if (lista[i].toLowerCase() == nomeBuscado.toLowerCase()) {
+      return true;
+    }
+  }
   return false;
 }
